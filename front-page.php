16c0c2b6f2ecc439e4f1133acf595f8591398033
AxelSanchez <sanchezax2024@gmail.com>
2025-04-23 13:59:54 +0200

    cambios en los links por botones

--- front-page.php
+++ front-page.php
@@ -3,8 +3,14 @@
 <main>
     <section>
         <div class="portada">
-            <div class="Titulo_portada"><h2>Bienvenido a mi blog</h2></div>
+            <div class="Titulo_portada">
+                <h2>Bienvenido a mi blog</h2>
+            </div>
             <img src="wp-content/uploads/2025/04/IMG_SAM0817.jpg" alt="">
+            <div class="botones">
+                <a href="">youtube</a> 
+                <a href="">instagram</a>
+        </div>
         </div>
     </section>
 </main>
